08f616a0e00d77be361307c26f3f81b8706a5cf7
github <github>
2025-04-05 15:17:02 +0900

    サイドバーにSNSボタン追加

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,26 @@
 <aside id="sidebar">
-<section class="side-item">
+<section class="side-item  author">
     <img src="<?php echo esc_url(get_theme_file_uri('img/auther.svg')); ?>" alt="作者プロフィール" class="author-img">
     <h3 class="side-title">Auther</h3>
     <p class="profile">
-    星と歴史が好きな有閑マダムを目指す普通の会社員です。こちらのブログは作者のオリジナルテーマ（ポートフォリオにて紹介）を使用しています。
+    星と歴史が好きな有閑マダムを目指す普通の会社員です。オリジナルテーマをデザインしながら記事を書いています。
     </p>
+    <ul>
+      <li>
+     <a href="https://x.com/devcosmo_luna">
+      <img src="<?php echo get_theme_file_uri('img/x-btn.png'); ?>" alt="x"></a>
+    </li>
+   <li>
+    <a href="https://www.instagram.com/devcosmo_biz/">
+      <img src="<?php echo get_theme_file_uri('img/insta-btn.png'); ?>" alt="instagram">
+     </a>
+   </li>
+   <li>
+     <a href="https://jp.pinterest.com/devmilk_biz/">
+      <img src="<?php echo get_theme_file_uri('img/pinterest-btn.png'); ?>" alt="pinterest">
+     </a>
+   </li>
+   </ul>
   </section>
 
   <section class="side-item">
